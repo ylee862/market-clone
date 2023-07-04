f7192ac95628f8ca50cdf7fbb78c7b2ac63947ad
ylee862 <damy03121y@gmail.com>
2023-07-05 07:10:21 +0900

    svelte, vite로 바꾼후 firebase deploy

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Login from "./pages/Login.svelte";
+  import Signup from "./pages/Signup.svelte";
+  import Wrtie from "./pages/Write.svelte";
+  import Main from "./pages/Main.svelte";
+  import Router from "svelte-spa-router";
+  import Write from "./pages/Write.svelte";
+  import NotFound from "./pages/NotFound.svelte";
+  import "./css/style.css";
+
+  const routes = {
+    "/": Main,
+    "/login": Login,
+    "/singup": Signup,
+    "/write": Write,
+    "*": NotFound,
+  };
+</script>
+
+<Router {routes} />
---
+++ src/pages/Login.svelte
@@ -0,0 +1,15 @@
+<form id="login-form" action="/signup" method="POST">
+  <div>로그인</div>
+  <div>
+    <label for="id">아이디</label>
+    <input type="text" id="id" name="id" required />
+  </div>
+  <div>
+    <label for="password">password</label>
+    <input type="password" id="password" name="password" required />
+  </div>
+  <div>
+    <button type="submit">login</button>
+  </div>
+  <div id="info" />
+</form>
